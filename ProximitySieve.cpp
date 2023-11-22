9a3eb7ecd25dba0815885147b89bc5edbbc54e91
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2023-11-21 17:53:08 -0700

    cpp: comments

--- ProximitySieve.cpp
+++ ProximitySieve.cpp
@@ -13,7 +13,7 @@ large primes, AND your protocol expects whole length n, then this sieve is OK.*/
 #include <fstream>
 #include <gmp.h> //For GNU Multiple Precision Arithmetic Library.
 //Do apt install libgmp-dev then append "-lgmp" to both compile &
-//build commands. Or then compile: g++ /path_to_tryRSA.cpp -lgmp
+//build commands. Or then compile: g++ /path_to_this.cpp -lgmp
 #include <iostream>
 using namespace std;
 
